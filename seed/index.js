120d073d05933f0055e9b6177d0d20ac4e08c302
ericuss <etorreg@gmail.com>
2023-02-01 18:45:15 +0100

    dockerize the js client

--- seed/index.js
+++ seed/index.js
@@ -1,75 +1,67 @@
 var mockServerClient = require('mockserver-client').mockServerClient;
 const { setTimeout } = require('timers/promises');
 
-var iso_server_host = 'localhost';
+// use env variables
+var iso_server_host = 'host.docker.internal';
 var iso_server_port = 1080;
 
 
 var sampleEndpoint = () => {
-    var path = '/api/sample';
-    var content = { name: "User001" };
-
-    var expectationBuilder = {
-        "httpRequest": {
-            "method": "GET",
-            "path": `${path}`
-        },
-        "httpResponse": {
-            "body": content
-        }
-    };
+  var expectationBuilder = {
+    "httpRequest": {
+      "method": "GET",
+      "path": '/api/sample'
+    },
+    "httpResponse": {
+      "body": { name: "User001" }
+    }
+  };
 
 
-    mockServerClient(iso_server_host, iso_server_port)
-        .mockAnyResponse(expectationBuilder)
-        .then(
-            () => {
-                console.log("requested endpoint");
-            },
-            (error) => console.log(error)
+  mockServerClient(iso_server_host, iso_server_port)
+    .mockAnyResponse(expectationBuilder)
+    .then(
+      () => {
+        console.log("requested endpoint");
+      },
+      (error) => console.log(error)
     );
 }
 
 var sampleEndpoint2 = () => {
-mockServerClient("localhost", 1080)
-  .mockAnyResponse({
-    "httpRequest": {
-      "method": "GET",
-      "path": "/view/cart",
-    },
-    "httpResponse": {
-      "body": "some_response_body"
-    }
-  })
-  .then(
-    function () {
-      console.log("expectation created");
-    },
-    function (error) {
-      console.log(error);
-    }
-  );
+  mockServerClient(iso_server_host, iso_server_port)
+    .mockAnyResponse({
+      "httpRequest": {
+        "method": "GET",
+        "path": "/view/cart",
+      },
+      "httpResponse": {
+        "body": "some_response_body"
+      }
+    })
+    .then(
+      function () {
+        console.log("expectation created");
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
 }
 
 
 var main = () => {
-    console.log("seeding endpoints")
-    sampleEndpoint();
-    sampleEndpoint2();
-    setTimeout(() => console.log("bye"), 5000);
+  console.log("seeding endpoints")
+  sampleEndpoint();
+  sampleEndpoint2();
+  setTimeout(() => console.log("bye"), 5000);
 }
 
 var bootstrapper = () => {
-    mockServerClient(iso_server_host, iso_server_port).reset().then(
-        () => main(),
-        () => setTimeout(bootstrapper, 10000)
-    )
+  mockServerClient(iso_server_host, iso_server_port).reset().then(
+    () => main(),
+    () => setTimeout(bootstrapper, 10000)
+  )
 }
 
 bootstrapper();
-
-async function sleep(t) {
-    await new Promise(resolve => setTimeout(resolve, t));
-}
-
-sleep(10000);
